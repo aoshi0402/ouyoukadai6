784f4ed1c65ce299bc06592083b02ab05f8b9c27
青島優 <aoshima.yu.0402@gmail.com>
2020-09-24 18:27:59 +0900

    first-commit

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,18 +1,16 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    book_comment = current_user.book_comments.new(book_comment_params)
-		book_comment.book_id = book.id
-    book_comment.save
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)
+		@book_comment.book_id = @book.id
+    @book_comment.save
   end
 
   def destroy
-    book = Book.find(params[:book_id])    
-    # book = BookComment.find(params[:id]).book
+    # book = Book.find(params[:book_id])    
+    @book = BookComment.find(params[:id]).book
     BookComment.find(params[:id]).destroy
-    redirect_back(fallback_location: root_path)
   end
 
   private
